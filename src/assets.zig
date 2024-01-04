ad024e222115d74fe63ec8d5c557ce5c6ca3077a
Gielnicki Aleksander <aleksander.gielnicki@grupawp.pl>
2024-01-04 17:28:09 +0100

    refactor: make assets top-level namespace

--- src/assets.zig
+++ src/assets.zig
@@ -1,24 +1,21 @@
 const std = @import("std");
-
 const bmp = @import("bmp.zig");
 
-pub const Assets = struct {
-    pub var bitmaps: std.StringHashMap(bmp.Bitmap) = undefined;
+pub var bitmaps: std.StringHashMap(bmp.Bitmap) = undefined;
 
-    pub fn init(allocator: std.mem.Allocator, bitmap_filenames: []const []const u8) !void {
-        bitmaps = std.StringHashMap(bmp.Bitmap).init(allocator);
-        for (bitmap_filenames) |filename| {
-            var basename_iter = std.mem.splitSequence(u8, filename, ".");
-            const basename = basename_iter.first();
-            try bitmaps.put(basename, try bmp.getPixelsFromBmp(allocator, filename));
-        }
+pub fn init(allocator: std.mem.Allocator, bitmap_filenames: []const []const u8) !void {
+    bitmaps = std.StringHashMap(bmp.Bitmap).init(allocator);
+    for (bitmap_filenames) |filename| {
+        var basename_iter = std.mem.splitSequence(u8, filename, ".");
+        const basename = basename_iter.first();
+        try bitmaps.put(basename, try bmp.getPixelsFromBmp(allocator, filename));
     }
+}
 
-    pub fn deinit(allocator: std.mem.Allocator) void {
-        var iter = bitmaps.iterator();
-        while (iter.next()) |bitmap| {
-            allocator.free(bitmap.value_ptr.pixels);
-        }
-        bitmaps.deinit();
+pub fn deinit(allocator: std.mem.Allocator) void {
+    var iter = bitmaps.iterator();
+    while (iter.next()) |bitmap| {
+        allocator.free(bitmap.value_ptr.pixels);
     }
-};
+    bitmaps.deinit();
+}
