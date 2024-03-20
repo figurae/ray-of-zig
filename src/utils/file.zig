c5b42953be338d88f9893437d0c598553f3d1dec
Gielnicki Aleksander <aleksander.gielnicki@grupawp.pl>
2024-03-20 18:19:22 +0100

    chore: adapt to Zig master

--- src/utils/file.zig
+++ src/utils/file.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 // NOTE: can the reader be typed better?
 pub fn readPackedStruct(reader: anytype, comptime T: type) !T {
-    comptime std.debug.assert(@typeInfo(T).Struct.layout == .Packed);
+    comptime std.debug.assert(@typeInfo(T).Struct.layout == .@"packed");
     comptime std.debug.assert(@bitSizeOf(T) % 8 == 0);
 
     var buffer: [@divExact(@bitSizeOf(T), 8)]u8 = undefined;
